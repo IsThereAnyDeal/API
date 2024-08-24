9931964327a597f078b64025ccde97c2e644be44
Tomáš Fedor <tomas@isthereanydeal.com>
2024-08-24 22:36:43 +0200

    Add new scopes

--- sdk/php/src/OAuth/EScope.php
+++ sdk/php/src/OAuth/EScope.php
@@ -4,6 +4,9 @@ namespace IsThereAnyDeal\SDK\OAuth;
 enum EScope: string
 {
     case UserInfo = "user_info";
+    case UserGames_Read = "notes_read";
+    case UserGames_Write = "notes_write";
+    case Profiles = "profiles";
     case WaitlistRead = "wait_read";
     case WaitlistWrite = "wait_write";
     case CollectionRead = "coll_read";
